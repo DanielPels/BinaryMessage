a9e514576f83bb0d03e99364c9306400507fc16f
Daniel <daniel@gobletgames.com>
2018-02-23 15:03:38 +0100

    Remember errors on binarymessage, no need to return every possible error.

--- binarymessage.go
+++ binarymessage.go
@@ -39,13 +39,14 @@ func (b *BinaryMessagePool) ReleaseBinaryMessage(m *BinaryMessage) {
 }
 
 type BinaryMessageInterface interface {
-	WriteInterface(interface{}) error
-	WriteIntAsUint8(int) error
-	WriteIntAsUint16(int) error
-	WriteFloatAsUint16(float64) error
-	WriteBytes([]byte) error
+	WriteInterface(interface{})
+	WriteIntAsUint8(int)
+	WriteIntAsUint16(int)
+	WriteFloatAsUint16(float64)
+	WriteBytes([]byte)
 	GetBuffer() *bytes.Buffer
 	GetBytes() []byte
+	HadError() bool
 	Reset()
 }
 
@@ -57,23 +58,38 @@ func NewBinaryMessage() *BinaryMessage {
 
 type BinaryMessage struct {
 	buffer *bytes.Buffer
+	error  error
 }
 
-func (m *BinaryMessage) WriteInterface(data interface{}) error {
-	return writeToBuffer(m.buffer, data)
+func (m *BinaryMessage) WriteInterface(data interface{}) {
+	err := writeToBuffer(m.buffer, data)
+	if err != nil {
+		m.error = err
+	}
 }
-func (m *BinaryMessage) WriteIntAsUint8(data int) error {
-	return writeToBuffer(m.buffer, uint8(data))
+func (m *BinaryMessage) WriteIntAsUint8(data int) {
+	err := writeToBuffer(m.buffer, uint8(data))
+	if err != nil {
+		m.error = err
+	}
 }
-func (m *BinaryMessage) WriteIntAsUint16(data int) error {
-	return writeToBuffer(m.buffer, uint16(data))
+func (m *BinaryMessage) WriteIntAsUint16(data int) {
+	err := writeToBuffer(m.buffer, uint16(data))
+	if err != nil {
+		m.error = err
+	}
 }
-func (m *BinaryMessage) WriteFloatAsUint16(data float64) error {
-	return writeToBuffer(m.buffer, uint16(math.Floor(data)))
+func (m *BinaryMessage) WriteFloatAsUint16(data float64) {
+	err := writeToBuffer(m.buffer, uint16(math.Floor(data)))
+	if err != nil {
+		m.error = err
+	}
 }
-func (m *BinaryMessage) WriteBytes(data []byte) error {
+func (m *BinaryMessage) WriteBytes(data []byte) {
 	_, err := m.buffer.Write(data)
-	return err
+	if err != nil {
+		m.error = err
+	}
 }
 func (m *BinaryMessage) GetBuffer() *bytes.Buffer {
 	return m.buffer
@@ -81,8 +97,15 @@ func (m *BinaryMessage) GetBuffer() *bytes.Buffer {
 func (m *BinaryMessage) GetBytes() []byte {
 	return m.buffer.Bytes()
 }
+func (m *BinaryMessage) HadError() bool {
+	if m.error != nil {
+		return true
+	}
+	return false
+}
 func (m *BinaryMessage) Reset() {
 	m.buffer.Reset()
+	m.error = nil
 }
 func writeToBuffer(buf *bytes.Buffer, data interface{}) error {
 	return binary.Write(buf, binary.LittleEndian, data)
